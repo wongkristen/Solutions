2418bfbf55fc95ea0403aab5ee089ac3e216522f
Kristen Wong <kristenwong@Kristens-MacBook-Pro.local>
2017-10-09 16:03:17 -0400

    Cleaned up main.cpp

--- main.cpp
+++ main.cpp
@@ -2,29 +2,8 @@
 #include <vector>
 #include "ref.h"
 
-
 using namespace std;
 
-
-
-
-
-
 int main(){
-    vector <int> nums;
-    nums.push_back(1);
-    nums.push_back(2);
-    nums.push_back(1);
-
-
-    IntToRomanSolution itr;
-    cout << "int to roman: " << itr.intToRoman(255);
-
-    vector<string> strings;
-    strings.push_back("aaaaaaaaaaaaaaaaaaaaaaaaaa");
-    strings.push_back("aaaaaaaaaaaaaaaaaaaaaa");
-    strings.push_back("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-    LongestCommonPrefixSolution lcp;
-    cout << endl << "longest common prefix: " << lcp.longestCommonPrefix(strings);
-
+    // ------ Debug and test here ------
 }
